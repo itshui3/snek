81caefa74ca955eb7b45a229e8e021b091a07676
Hui <jeffrey.x.hui@gmail.com>
2020-09-16 20:56:47 -0700

    set_dir logic seems pretty complete, next step is to write logic that mutates board

--- src/page/SnekBoard.js
+++ src/page/SnekBoard.js
@@ -20,7 +20,7 @@ const SnekBoard = () => {
 
     const [prep, setPrep] = useState(false)
     const [snek, setSnek] = useState(false)
-    const [direction, setDirection] = useState(false)
+    const [dir, setDir] = useState(false)
 
     const window = useRef(document.window)
     useEffect(() => {
@@ -38,6 +38,17 @@ const SnekBoard = () => {
 
     }, [prep])
 
+    useEffect(() => {
+        if (
+            dir === 'e' ||
+            dir === 's' ||
+            dir === 'd' ||
+            dir === 'f'
+        ) {
+            // moveSnek()
+        }
+    }, [dir])
+
     const startGame = () => {
         setPrep(!prep)
     }
@@ -49,10 +60,10 @@ const SnekBoard = () => {
         setSnek(true)
     }
 
-    const setDir = (key, ev) => {
+    const setDirection = (key, ev) => {
         if (!prep || !snek) { return }
         console.log('key: ', key)
-        setDirection((dir) => {
+        setDir((dir) => {
 
             if (key === 'e') return 'e'
             if (key === 's') return 's'
@@ -81,7 +92,7 @@ const SnekBoard = () => {
                 ? 'Click Start to begin'
                 : prep && !snek 
                 ? 'Place ur snek boyo' 
-                : prep && snek && !direction
+                : prep && snek && !dir
                 ? 'Move ur snek boyo [e(north), s(west), d(south), f(east)]'
                 : null
                 }
@@ -90,7 +101,7 @@ const SnekBoard = () => {
             {/* directions controller */}
             <KeyBoardEventHandler 
             handleKeys={['e', 's', 'd', 'f']}
-            onKeyEvent={(key, ev) => setDir(key, ev)}
+            onKeyEvent={(key, ev) => setDirection(key, ev)}
             />
             <div className='grid'>
             {
